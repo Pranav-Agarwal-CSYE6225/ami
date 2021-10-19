fea927e520fbc698c12c98bd6837d308e12454a6
Pranav-Agarwal <agarwal.pran@northeastern.edu>
2021-10-19 01:14:35 -0400

    Created packer HCL script to build AMI for a node webapp
    Script will use a base Ubuntu 20 image, and install node, npm and sql. It will then create the necessary table in sql, copy the source zip to the image, extract it and install dependencies using npm install.

---
+++ setup.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "==> Installing node version manager (NVM)."
+
+mkdir ~/Documents
+mkdir ~/Documents/server
+mv /tmp/webapp.zip ~/Documents/server
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.0/install.sh | bash
+source ~/.bashrc
+source ~/.nvm/nvm.sh
+
+nvm install 14
+npm install -g npm@latest
+
+#update apt
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install unzip
+cd ~/Documents/server/ && unzip webapp.zip
+cd ~/Documents/server/webapp-main && npm install
+
+# Install mysql
+sudo apt-get install mysql-community-client-plugins mysql-common
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+
+
+sudo mysql --user="root" --password="" --execute='CREATE USER '\''phpmyadmin'\''@'\''localhost'\'' IDENTIFIED BY "root";'
+
+sudo mysql --user="root" --password="" --execute='ALTER USER '\''phpmyadmin'\''@'\''localhost'\'' IDENTIFIED WITH mysql_native_password BY "root";'
+
+sudo mysql --user="root" --password="" --execute='CREATE DATABASE csye6225;'
+
+sudo mysql --user="root" --password="" --execute='GRANT ALL PRIVILEGES ON csye6225.* TO '\''phpmyadmin'\''@'\''localhost'\'' ;'
+
+sudo mysql --user="root" --password="" --execute='flush privileges;'
+
+sudo mysql --user="phpmyadmin" --password="root" --database='csye6225' --execute="CREATE TABLE user (
+  id int NOT NULL,
+  first_name varchar(100) NOT NULL,
+  last_name varchar(100) NOT NULL,
+  username varchar(100) NOT NULL,
+  password varchar(100) NOT NULL,
+  account_created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  account_updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ALTER TABLE user
+  ADD PRIMARY KEY (id,username);
+ALTER TABLE user
+  MODIFY id int NOT NULL AUTO_INCREMENT;"
---
+++ variables.pkr.hcl
@@ -0,0 +1,21 @@
+variable "ami_name" {
+  type =  string
+}
+variable "instance_type" {
+  type =  string
+}
+variable "region" {
+  type =  string
+}
+variable "profile" {
+  type =  string
+}
+variable "source_ami" {
+  type =  string
+}
+variable "ssh_username" {
+  type =  string
+}
+variable "ami_description" {
+  type =  string
+}
