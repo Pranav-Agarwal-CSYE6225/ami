20aeeeec25ed1823d3c7c4ada2c8c2fd13a8a7c5
Pranav-Agarwal <agarwal.pran@northeastern.edu>
2021-11-03 03:01:25 -0400

    updated setup.bash

--- setup.bash
+++ setup.bash
@@ -21,9 +21,9 @@ wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/releases/codedeploy-agent
 mkdir codedeploy-agent_1.0-1.1597_ubuntu20
 dpkg-deb -R codedeploy-agent_1.0-1.1597_all.deb codedeploy-agent_1.0-1.1597_ubuntu20
 sed 's/2.0/2.7/' -i ./codedeploy-agent_1.0-1.1597_ubuntu20/DEBIAN/control
-dpkg-deb -b codedeploy-agent_1.0-1.1597_ubuntu20
-dpkg -i codedeploy-agent_1.0-1.1597_ubuntu20.deb
-systemctl start codedeploy-agent
-systemctl enable codedeploy-agent
+sudo dpkg-deb -b codedeploy-agent_1.0-1.1597_ubuntu20
+sudo dpkg -i codedeploy-agent_1.0-1.1597_ubuntu20.deb
+sudo service codedeploy-agent start
+sudo systemctl enable codedeploy-agent
 
 echo "==> Completed Provisioning!."
